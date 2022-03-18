106150e955b32a76e40d253d9442d215c92e9226
ateostudio <101821825+ateostudio@users.noreply.github.com>
2022-03-18 22:42:32 +0700

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -45,6 +45,7 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
+users UserName:CL:Password
 maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
@@ -55,17 +56,8 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6291456 
-flush
 auth strong
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
-}
+flush
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
